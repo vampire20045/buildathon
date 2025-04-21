c24e9405eee43a2f2567422178449f6e6471db64
hardik yadav <hardikyadav1182005@gmail.com>
2025-04-22 00:29:32 +0530

    websocket ai

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Singup } from './newcompo/signup'
 import { Singin } from './newcompo/signin'
 import { Homepage } from './newcompo/homepage'
+import { InterviewPage } from './newcompo/interview'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<Homepage/>}></Route>
-
+    <Route path="/Interview" element={<InterviewPage/>}></Route>
       <Route path="/Singup" element={<Singup/>}></Route>
       <Route path="/Signin" element={<Singin/>}></Route>
      
