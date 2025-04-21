4ad757ce714d9dae8776db514905c0470a53361e
hardik yadav <hardikyadav1182005@gmail.com>
2025-04-21 15:14:33 +0530

    commit

--- frontend/my-app/src/newcompo/homepage.tsx
+++ frontend/my-app/src/newcompo/homepage.tsx
@@ -1,9 +1,17 @@
+import Navbar from "./subcomponent/navbar"
+
 export const Homepage=()=>{
     return(
         <div className="bg-black ">
+            {/* this is the vanta div need to ad  bg 3d */}
             <div className="h-screen w-full text-white  ">
-<h1>how areyou </h1>
+<div className="h-18 w-full ">
+    <Navbar></Navbar>
+
+</div>
 </div> 
+
+
         </div>
     )
 }
